88faaf3debbc0597b0f863b87ebc83d5900cb8c2
naveenkumar561 <118717645+naveenkumar561@users.noreply.github.com>
2024-11-15 12:49:05 +0530

    Update python_word.py

--- python_word.py
+++ python_word.py
@@ -1,18 +1,18 @@
-def starts_with_b(s):
-    return s[0].lower() == 'b'
+def starts_with(name, char):
+    return name[0].lower() == char.lower()
 
-def ends_with_b(s):
-    return s[-1].lower() == 'b'
+def ends_with(name, char):
+    return name[-1].lower() == char.lower()
 
-def filter_strings(names, predicate):
+def filter_strings(names, predicate, char=None):
+    if char:
+        return [name for name in names if predicate(name, char)]
     return [name for name in names if predicate(name)]
 
-names = ["Bosch", "bengaluru", "lenovo", "BoB"]
+names = ["Bosch", "bengaluru", "lenovo", "Bob"]
+letter = 'b'
+filtered_names_start = filter_strings(names, starts_with, letter)
+filtered_names_end = filter_strings(names, ends_with, letter)
 
-# Filter names that start with 'b'
-filtered_names_start = filter_strings(names, starts_with_b)
-print("starts_with_b:", filtered_names_start)
-
-# Filter names that end with 'b'
-filtered_names_end = filter_strings(names, ends_with_b)
-print("ends_with_b:", filtered_names_end)
+print(f"Filtered names starting with {letter}:", filtered_names_start)
+print(f"Filtered names ending with {letter}:", filtered_names_end)
