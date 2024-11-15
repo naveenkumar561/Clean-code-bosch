c1a4feafec2577c4123f73c395ef3ad74affc443
naveenkumar561 <118717645+naveenkumar561@users.noreply.github.com>
2024-11-15 11:11:17 +0530

    Update python_word.py
    pure function and functional programming

--- python_word.py
+++ python_word.py
@@ -1,8 +1,16 @@
-def filter_words(words, character):
-    return [word for word in words if word and word[0].lower() == character]
+from typing import List, Callable
+Predicate = Callable[[str], bool]
+def starts_with_char(character: str) -> Predicate:
+    return lambda word: word and word[0].lower() == character.lower()
+
+def filter_words(words: List[str], predicate: Predicate) -> List[str]:
+    return list(filter(predicate, words))
 
 words = ["Bosch", "bengaluru", "lenovo", "Rusin"]
 character = 'b'
-result = filter_words(words, character)
+
+predicate = starts_with_char(character)
+
+result = filter_words(words, predicate)
 
 print(result)
